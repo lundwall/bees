730312e2ffa399b53900ffe8b32e53a496cad9b5
lundwall <23060984+lundwall@users.noreply.github.com>
2023-06-22 15:21:03 -0300

    Add Arton script

---
+++ arton.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --output=%j.out     # where to store the output (%j is the JOBID), subdirectory "log" must exist
+#SBATCH --cpus-per-task=8
+
+# Exit on errors
+set -o errexit
+
+# # Set a directory for temporary files unique to the job with automatic removal at job termination
+# TMPDIR=$(mktemp -d)
+# if [[ ! -d ${TMPDIR} ]]; then
+#     echo 'Failed to create temp directory' >&2
+#     exit 1
+# fi
+# trap "exit 1" HUP INT TERM
+# trap 'rm -rf "${TMPDIR}"' EXIT
+# export TMPDIR
+
+# # Change the current directory to the location where you want to store temporary files, exit if changing didn't succeed.
+# # Adapt this to your personal preference
+# cd "${TMPDIR}" || exit 1
+
+# Send some noteworthy information to the output log
+echo "Running on node: $(hostname)"
+echo "In directory:    $(pwd)"
+echo "Starting on:     $(date)"
+echo "SLURM_JOB_ID:    ${SLURM_JOB_ID}"
+
+# Binary or script to execute
+/itet-stor/mlundwall/net_scratch/conda_envs/bees/bin/python /home/mlundwall/bees/rllib_train.py
+
+# Send more noteworthy information to the output log
+echo "Finished at:     $(date)"
+
+# End the script with exit code 0
+exit 0
