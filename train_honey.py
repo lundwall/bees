d205fe4d17b5f2ec42bd713b283c3c2bd4b73922
lundwall <23060984+lundwall@users.noreply.github.com>
2023-06-23 17:08:20 +0200

    Run on Arton

--- train_honey.py
+++ train_honey.py
@@ -1,3 +1,4 @@
+import ray
 from ray.tune.registry import register_env
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray import air, tune
@@ -6,23 +7,11 @@ from ray.air.integrations.wandb import WandbLoggerCallback
 import honey.environment as environment
 # import rllib pettingzoo interface
 from pettingzoo_env import PettingZooEnv
+
 # define how to make the environment. This way takes an optional environment config
 env_creator = lambda config: environment.env()
 # register that way to make the environment under an rllib name
 register_env('environment', lambda config: PettingZooEnv(env_creator(config)))
-
-# algo = PPOConfig().environment('environment').build()
-
-# # Tuning hyper-parameters
-# tune.run(
-#     "PPO",
-#     config={
-#         # define search space here
-#         "lr": tune.uniform(1e-3, 1e-7),
-#         "train_batch_size": tune.randint(1_000, 10_000),
-#     },
-#     callbacks=[WandbLoggerCallback(project="bees", api_key_file="/Users/marclundwall/.wandb_api_key", log_config=True)])
-#     # loggers=DEFAULT_LOGGERS + (WandbLoggerCallback, ))
 
 config = PPOConfig()
 # config = config.training(
@@ -31,14 +20,14 @@ config = PPOConfig()
 # )
 config = config.environment('environment')
 
-saved_results = f"~/ray_results/PPO"
+saved_results = "~/ray_results/PPO"
 if tune.Tuner.can_restore(saved_results):
     tuner = tune.Tuner.restore(saved_results, trainable="PPO", resume_errored=True)
 else:
     tuner = tune.Tuner(
         "PPO",
         run_config=air.RunConfig(
-            stop={"timesteps_total": 1_000_000},
+            stop={"timesteps_total": 100_000},
             callbacks=[WandbLoggerCallback(project="bees", api_key_file="~/.wandb_api_key", log_config=True)],
         ),
         tune_config=tune.TuneConfig(
@@ -48,6 +37,8 @@ else:
     )
 results = tuner.fit()
 
+# Train without tuning
+# algo = PPOConfig().environment('environment').build()
 # for i in range(10001):
 #     algo.train()
 #     if i % 100 == 0:
