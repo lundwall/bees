58f01ce2928bfae243bfb24d180ecbdb5fac1c84
lundwall <23060984+lundwall@users.noreply.github.com>
2023-06-23 16:02:56 -0300

    Limit number of CPUs

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import environment as environment
 from pettingzoo_env import PettingZooEnv
 
 # Limit number of cores
-# ray.init(num_cpus=16)
+ray.init(num_cpus=16)
 
 # define how to make the environment. This way takes an optional environment config
 env_creator = lambda config: environment.env()
