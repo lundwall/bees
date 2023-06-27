ba8a2054597db45d0cc2a4926ea5cd1714656d2a
lundwall <23060984+lundwall@users.noreply.github.com>
2023-06-26 23:03:25 -0300

    Commit train

--- train.py
+++ train.py
@@ -22,18 +22,18 @@ config = config.training(
     model={
         "custom_model": TorchActionMaskModel,
     },
-    lr=5e-5,#tune.grid_search([i * 1e-5 for i in range(1, 11, 2)]),
-    train_batch_size=5000,#tune.grid_search(list(range(1000, 11_001, 2000))),
+    # lr=5e-5,#tune.grid_search([i * 1e-5 for i in range(1, 11, 2)]),
+    # train_batch_size=5000,#tune.grid_search(list(range(1000, 11_001, 2000))),
 )
 config = config.environment('environment')
 
 tuner = tune.Tuner(
     "PPO",
     run_config=air.RunConfig(
-        name="lr7,tbs",
+        name="trace",
         #local_dir="/itet-stor/mlundwall/net_scratch/ray_results",
         local_dir="/Users/marclundwall/ray_results",
-        stop={"timesteps_total": 100_000},
+        stop={"timesteps_total": 10_000_000},
         callbacks=[WandbLoggerCallback(project="bees", api_key_file="~/.wandb_api_key", log_config=True)],
         checkpoint_config=air.CheckpointConfig(
             checkpoint_frequency=1000,
